91f6f5e0f9facbb35d9e1e183fad8d264aaec585
Maria Amanda <mariaaraujo.amanda@gmail.com>
2021-12-29 01:30:39 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -4,37 +4,53 @@ namespace Hcode;
 
 class Model {
 
-	privete $values = [];
+	private $values = [];
+
+	public function setData($data)
+	{
+
+		foreach ($data as $key => $value)
+		{
+
+			$this->{"set".$key}($value);
+
+		}
+
+	}
 
 	public function __call($name, $args)
 	{
+
 		$method = substr($name, 0, 3);
 		$fieldName = substr($name, 3, strlen($name));
 
-		switch ($method) 
+		if (in_array($fieldName, $this->fields))
 		{
-			case "get":
-				return $this->values[$fieldName];
-			break;
 			
-			case "set":
-				$this->values[$fieldName] = $args[0];
-			break;
-		}
-	}
+			switch ($method)
+			{
 
-	public function setData($date = array())
-	{
-		foreach ($data as $key => $value) 
-		{
-			$this->{"set".$key}($value);
+				case "get":
+					return $this->values[$fieldName];
+				break;
+
+				case "set":
+					$this->values[$fieldName] = $args[0];
+				break;
+
+			}
+
 		}
+
 	}
 
 	public function getValues()
 	{
+
 		return $this->values;
+
 	}
+
 }
 
  ?>

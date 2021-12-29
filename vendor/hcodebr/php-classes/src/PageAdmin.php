4aafee1fe73adeb313c7a410cbc77281ebdfa0f6
Maria Amanda <mariaaraujo.amanda@gmail.com>
2021-12-29 00:26:59 -0300

    criamos pageadmin e tpl admin

--- vendor/hcodebr/php-classes/src/PageAdmin.php
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -2,13 +2,15 @@
 
 namespace Hcode;
 
-class PageAdmin extends Page{
+class PageAdmin extends Page {
 
 	public function __construct($opts = array(), $tpl_dir = "/views/admin/")
 	{
+
 		parent::__construct($opts, $tpl_dir);
+
 	}
 
 }
 
- ?>
+?>

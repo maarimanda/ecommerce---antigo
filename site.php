39e9043315c1f44c27e80e1d3b35815de5031ac4
Maria Amanda <mariaaraujo.amanda@gmail.com>
2022-01-28 01:21:36 -0300

    Categorias VS Produtos

--- site.php
+++ site.php
@@ -1,7 +1,8 @@
 <?php
 
 use \Hcode\Page;
-use \Hcode\Model\Product; 
+use \Hcode\Model\Product;
+use \Hcode\Model\Category;
 
 $app->get('/', function() {
     
@@ -15,6 +16,19 @@ $app->get('/', function() {
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
 
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>Product::checkList($category->getProducts())
+	]);
+
+});
 
  ?>

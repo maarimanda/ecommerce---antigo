dbcb966bfa75a65ae9d03d83086b3df60b120f61
Maria Amanda <mariaaraujo.amanda@gmail.com>
2022-01-30 23:00:47 -0300

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -103,6 +103,35 @@ class Category extends Model {
 		}
 	}
 
+	public function getProductsPage($page = 1, $itemsPerPage = 8)
+	{
+
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT sql_calc_found_rows *
+			FROM tb_products a 
+			INNER JOIN tb_productscategories b ON b.idproduct = a.idproduct
+			INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+			WHERE c.descategory = :idcategory
+			LIMIT $start, $itemsPerPage;
+			
+		", [
+			':idcategory'=>$this->getidcategory()
+		]);
+
+		$resultTotal = $sql->select("SELECT found_rows() AS nrtotal");
+
+		return [
+			'data'=>Product::checkList($results),
+			'total'=>(int)$resultsTotal[0]["nrtotal"],
+			'pages'=>ceil($resultsTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+
+	}
+
 	public function addProduct(Product $product)
 	{
 
